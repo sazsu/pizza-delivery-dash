aa19581c1119d073c236d84176c7003a0592fafe
sazsu <149969207+sazsu@users.noreply.github.com>
2025-01-31 01:16:04 +0300

    feat(config): define temporary `BUILDING_LAYER` constant

--- src/config.py
+++ src/config.py
@@ -2,3 +2,23 @@ class Config:
     FPS = 60
     WIDTH = HEIGHT = 1024
     TILES_PER_SCREEN_VERT = 16
+    BUILDING_LAYER = frozenset(
+        [
+            'a',
+            'b',
+            'c',
+            'd',
+            'i',
+            'j',
+            'k',
+            'l',
+            'lwq',
+            'wq',
+            'rwq',
+            'lm',
+            'n',
+            'o',
+            'rm',
+            'p',
+        ]
+    )
